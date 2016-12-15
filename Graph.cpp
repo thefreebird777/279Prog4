7f55d1fb96046d15247283babb6fdcf291064d8e
Ian <ileyden96@gmail.com>
2016-12-15 16:26:01 -0600

    finished creating adjacency list for an unweighted graph

--- Graph.cpp
+++ Graph.cpp
@@ -1,10 +1,10 @@
-#include <rpcndr.h>
+//#include <rpcndr.h>
 #include <iostream>
 #include <fstream>
 #include "Graph.h"
 
 struct Node {
-    int data;
+    int adj;
     Node *next;
 };
 struct List {
@@ -13,34 +13,55 @@ struct List {
 
 class Graph {
     int vertices;
-    struct List *adjacencyList;
+    struct List *vertexArray;
 
 public:
     Graph(int vertices) {
         this->vertices = vertices;
-        adjacencyList = new List[vertices];
+        vertexArray = new List[vertices];
         for (int i = 0; i < vertices; i++) {
-            adjacencyList[i].head = NULL;
+            vertexArray[i].head = NULL;
         }
     }
 
-    Node *newAdjListNode(int data) {
-        Node *nextNode = new Node;
-        nextNode->data = data;
-        nextNode->next = NULL;
-        return nextNode;
+    Node *nextNode(int adj) {
+        Node *newNode = new Node;
+        newNode->adj = adj;
+        newNode->next = NULL;
+        return newNode;
     }
 
-    boolean ReadGraph(char *arg[]) {
-        std::ifstream inputFile(arg[1]);
-        if (!inputFile.is_open()) {
-            std::cout << "Error opening file " << arg[1] << std::endl;
-        } else {
-            while (!inputFile.eof()) {
-                //TODO
+    void newEdge(int vert, int adj) {
+        Node *newNode = nextNode(adj);
+        newNode->next = vertexArray[vert].head;
+        vertexArray[vert].head = newNode;
+        newNode = nextNode(vert);
+        newNode->next = vertexArray[adj].head;
+        vertexArray[adj].head = newNode;
+    }
+
+    void test(){
+        for (int i = 1; i < 6; i++) {
+            Node *printNode = vertexArray[i].head;
+            std::cout<<i;
+            while (printNode) {
+                std::cout << "->" << printNode->adj;
+                printNode = printNode->next;
             }
+            std::cout<<std::endl;
         }
     }
+
+//    boolean ReadGraph(char *arg[]) {
+//        std::ifstream inputFile(arg[1]);
+//        if (!inputFile.is_open()) {
+//            std::cout << "Error opening file " << arg[1] << std::endl;
+//        } else {
+//            while (!inputFile.eof()) {
+//                //TODO
+//            }
+//        }
+//    }
 
     void TopologicalSort() {}
 
@@ -50,6 +71,24 @@ public:
 };
 
 int main() {
+    Graph *graph = new Graph(5);
+
+    graph->nextNode(1);
+    graph->nextNode(2);
+    graph->nextNode(3);
+    graph->nextNode(4);
+    graph->nextNode(5);
+
+    graph->newEdge(1, 2);
+    graph->newEdge(1, 4);
+    graph->newEdge(2, 3);
+    graph->newEdge(4, 5);
+    graph->newEdge(1, 3);
+    graph->newEdge(2, 4);
+
+    graph->test();
+
+
 
     return 0;
 }
